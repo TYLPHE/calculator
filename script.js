8a51f271e4f16db3ef5aff316367f6f5a5a97be0
TYLPHE <tylphet@gmail.com>
2021-09-08 00:17:47 -0600

    fixed operators and eq button and added multiple functions when spamming operator

--- script.js
+++ script.js
@@ -24,11 +24,16 @@ let memory = null;
 let operator;
 let windowValue = "0";
 win.textContent = windowValue;
-
+const formatter = new Intl.NumberFormat(`en-US`, {
+    minimumFractionDigits:0,
+    maximumFractionDigits:7,
+});
 ac.addEventListener(`click`, function(){
+    win.style.color = `white`;
     windowValue = `0`;
     value1 = null;
     value2 = null;
+    memory = null;
     win.textContent = windowValue;
 });
 del.addEventListener(`click`, function(){
@@ -38,162 +43,226 @@ del.addEventListener(`click`, function(){
     win.textContent = windowValue;
 });
 butt1.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `1`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `1`;
+    else{
+        windowValue += `1`;
+    }
+    
     win.textContent = windowValue;
 });
 butt2.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `2`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `2`;
+    else{
+        windowValue += `2`;
+    }
     win.textContent = windowValue;
 });
 butt3.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `3`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `3`;
+    else{
+        windowValue += `3`;
+    }
     win.textContent = windowValue;
 });
 butt4.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `4`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `4`;
+    else{
+        windowValue += `4`;
+    }
     win.textContent = windowValue;
 });
 butt5.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `5`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `5`;
+    else{
+        windowValue += `5`;
+    }
     win.textContent = windowValue;
 });
 butt6.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `6`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `6`;
+    else{
+        windowValue += `6`;
+    }
     win.textContent = windowValue;
 });
 butt7.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `7`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `7`;
+    else{
+        windowValue += `7`;
+    }
     win.textContent = windowValue;
 });
 butt8.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `8`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `8`;
+    else{
+        windowValue += `8`;
+    }
     win.textContent = windowValue;
 });
 butt9.addEventListener(`click`, function(){
-    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = ``;
-    windowValue += `9`;
+    win.style.color = `white`;
+    if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `9`;
+    else{
+        windowValue += `9`;
+    }
     win.textContent = windowValue;
 });
 butt0.addEventListener(`click`, function(){
+    win.style.color = `white`;
     if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `0`;
-    if (windowValue.includes(`.`) || windowValue == ``){
+    // if (windowValue.includes(`.`) || windowValue == ``){
+    else{
         windowValue += `0`;
     }
     win.textContent = windowValue;
 });
 dot.addEventListener(`click`, function(){
+    win.style.color = `white`;
     if (windowValue == 0 && !windowValue.includes(`.`)) windowValue = `0`;
-    if(windowValue.includes(`.`)) return;
-    windowValue += `.`;
+    if (windowValue.includes(`.`)) return;
+    else windowValue += `.`;
     win.textContent = windowValue;
 });
 plu.addEventListener(`click`, function(){
-    operator = `+`;
+    win.style.color = `yellow`;
     if (value1 == null){
+        operator = `+`;
         value1 = Number(windowValue);
-        windowValue = `0`
-        win.textContent = windowValue;
+        windowValue = `0`;
+        win.textContent = value1;
         return;
     }
-    if (value1 !== null){
-        value2 = Number(windowValue);
-    }
-    if(value1 !== null && value2 !== null){
-        memory = Number(value1) + Number(value2);
-        value1 = memory;
-        value2 = null;
+    if(value1 !== null){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
+        operator = `+`;
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = value1;
     }
 });
 min.addEventListener(`click`, function(){
-    operator = `-`;
+    win.style.color = `yellow`;
     if (value1 == null){
+        operator = `-`;
         value1 = Number(windowValue);
         windowValue = `0`
-        win.textContent = windowValue;
+        win.textContent = value1;
         return;
     }
-    if (value1 !== null){
-        value2 = Number(windowValue);
-    }
-    if(value1 !== null && value2 !== null){
-        memory = Number(value1) - Number(value2);
-        value1 = memory;
-        value2 = null;
+    if(value1 !== null){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
+        operator = `-`;
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = value1;
     }
 });
 mul.addEventListener(`click`, function(){
-    operator = `*`;
+    win.style.color = `yellow`;
     if (value1 == null){
+        operator = `*`;
         value1 = Number(windowValue);
         windowValue = `0`
-        win.textContent = windowValue;
+        win.textContent = value1;
         return;
     }
-    if (value1 !== null){
-        value2 = Number(windowValue);
-    }
-    if(value1 !== null && value2 !== null){
-        memory = Number(value1) * Number(value2);
-        value1 = memory;
-        value2 = null;
+    if(value1 !== null){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
+        operator = `*`;
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = formatter.format(value1);
     }
 });
 div.addEventListener(`click`, function(){
-    operator = `/`;
+    win.style.color = `yellow`;
     if (value1 == null){
+        operator = `/`;
         value1 = Number(windowValue);
         windowValue = `0`
-        win.textContent = windowValue;
+        win.textContent = value1;
         return;
     }
-    if (value1 !== null){
-        value2 = Number(windowValue);
-    }
-    if(value1 !== null && value2 !== null){
-        memory = Number(value1) / Number(value2);
-        value1 = memory;
-        value2 = null;
+    if(value1 !== null){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
+        operator = `/`;
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = formatter.format(value1);
     }
 });
 equ.addEventListener(`click`, function(){
-    if(value1 != null){
-        value2 = Number(windowValue);
-    }
-    if (operator = `+`){
-        memory = Number(value1) + Number(value2);
+    win.style.color = `white`;
+    if (operator == `+`){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = formatter.format(value1);
     }
-    if (operator = `-`){
-        memory = Number(value1) - Number(value2);
+    else if (operator == `-`){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = formatter.format(value1);
     }
-    if (operator = `/`){
-        memory = Number(value1) / Number(value2);
+    else if (operator == `/`){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
         windowValue = `0`;
-        win.textContent = memory;
+        win.textContent = formatter.format(value1);
     }
-    if (operator = `*`){
-        memory = Number(value1) * Number(value2);
+    else if (operator == `*`){
+        if(windowValue == `0`){
+            value2 = memory;
+        }
+        else value2 = Number(windowValue);
+        memory = value2;
+        value1 = eval(value1 + operator +  value2);
         windowValue = `0`;
-        win.textContent = memory;
-
+        win.textContent = formatter.format(value1);
     }
+    else{
+        windowValue = `equal function error`;
+        win.textContent = windowValue;
+    }
+    value1 = null;
+    value2 = null;
+    memory = null;
+    return;
 });
